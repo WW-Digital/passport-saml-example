bbfa2c72f1d6f7fe62b0c0b35e5a7a9235f04407
Tim King <tim.king@weightwatchers.com>
2016-09-08 16:18:03 -0700

    Configure for Sailpoint SSO

--- config/config.js
+++ config/config.js
@@ -21,9 +21,9 @@ module.exports = {
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
         entryPoint: process.env.SAML_ENTRY_POINT || 'https://stg01-useast1-sso.identitynow.com/sso/SSORedirect/metaAlias/ww-sandbox/idp',
-        issuer: 'passport-saml',
+        issuer: process.env.SAML_ISSUER || 'passport-saml',
         cert: process.env.SAML_CERT || fs.readFileSync('./cert-qat2.pem', 'utf-8'),
-        logoutUrl: 'https://ww-sandbox.identitynow.com/logout'
+        logoutUrl: process.env.SAML_LOGOUT_URL || 'https://ww-sandbox.identitynow.com/logout'
       },
       roleMapper: roleMapper('OU=whisper,OU=Groups,OU=Site-Independent,OU=ManagedOU,DC=NA,DC=weightwatchers,DC=net',
         {
