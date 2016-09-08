c11131f37fcb1ad85b2d1cca2d02df5f713eb591
Tim King <tim.king@weightwatchers.com>
2016-09-08 16:12:32 -0700

    Configure for Sailpoint SSO

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-var roleMapper = function(groupFilter, groupToRole) {
-  return function(groups) {
+var roleMapper = function (groupFilter, groupToRole) {
+  return function (groups) {
     return groups.filter(function (entry) {
       return entry.indexOf(groupFilter) > -1;
     }).map(function (entry) {
@@ -26,11 +26,11 @@ module.exports = {
         logoutUrl: 'https://ww-sandbox.identitynow.com/logout'
       },
       roleMapper: roleMapper('OU=whisper,OU=Groups,OU=Site-Independent,OU=ManagedOU,DC=NA,DC=weightwatchers,DC=net',
-          {
-            'whisper_dev_editor': 'editor',
-            'whisper_dev_admin': 'admin',
-            'whisper_dev_viewer': 'viewer'
-          })
+        {
+          'whisper_dev_editor': 'editor',
+          'whisper_dev_admin': 'admin',
+          'whisper_dev_viewer': 'viewer'
+        })
     }
   }
 };
