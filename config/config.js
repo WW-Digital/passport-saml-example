176eb1621a6111ea1dedddf4ec10d6974beb7ae9
Tim King <tim.king@weightwatchers.com>
2016-11-02 09:58:18 -0700

    Add missing dependency

--- config/config.js
+++ config/config.js
@@ -10,27 +10,27 @@ var roleMapper = function (groupFilter, groupToRole) {
   }
 };
 
-module.exports = {
-  development: {
-    app: {
-      name: 'Passport SAML strategy example',
-      port: process.env.PORT || 8001
-    },
-    passport: {
-      strategy: 'saml',
-      saml: {
-        path: process.env.SAML_PATH || '/login/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'https://stg01-useast1-sso.identitynow.com/sso/SSORedirect/metaAlias/ww-sandbox/idp',
-        issuer: process.env.SAML_ISSUER || 'passport-saml',
-        cert: process.env.SAML_CERT || fs.readFileSync('./cert-qat2.pem', 'utf-8'),
-        logoutUrl: process.env.SAML_LOGOUT_URL || 'https://ww-sandbox.identitynow.com/logout'
+  module.exports = {
+    development: {
+      app: {
+        name: 'Passport SAML strategy example',
+        port: process.env.PORT || 8001
       },
-      roleMapper: roleMapper('OU=whisper,OU=Groups,OU=Site-Independent,OU=ManagedOU,DC=NA,DC=weightwatchers,DC=net',
-        {
-          'whisper_dev_editor': 'editor',
-          'whisper_dev_admin': 'admin',
-          'whisper_dev_viewer': 'viewer'
-        })
+      passport: {
+        strategy: 'saml',
+        saml: {
+          path: process.env.SAML_PATH || '/login/callback',
+          entryPoint: process.env.SAML_ENTRY_POINT || 'https://stg01-useast1-sso.identitynow.com/sso/SSORedirect/metaAlias/ww-sandbox/idp',
+          issuer: process.env.SAML_ISSUER || 'passport-saml',
+          cert: process.env.SAML_CERT || fs.readFileSync('./cert-qat2.pem', 'utf-8'),
+          logoutUrl: process.env.SAML_LOGOUT_URL || 'https://ww-sandbox.identitynow.com/logout'
+        },
+        roleMapper: roleMapper('OU=whisper,OU=Groups,OU=Site-Independent,OU=ManagedOU,DC=NA,DC=weightwatchers,DC=net',
+          {
+            'whisper_dev_editor': 'editor',
+            'whisper_dev_admin': 'admin',
+            'whisper_dev_viewer': 'viewer'
+          })
+      }
     }
-  }
-};
+  };
